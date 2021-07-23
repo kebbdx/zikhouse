30382f9aa102ddbaa2da79c18084a60a64dc87d3
kebbdx <bensalah.kamelleddine@gmail.com>
2021-07-23 09:07:47 +0200

    security profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class UserController extends AbstractController
-{
+{ 
     /**
      * @Route("/profile", name="user_profile")
      */
